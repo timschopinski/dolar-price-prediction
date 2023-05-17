24e502acf4ba803257398630e8861432e6f301fb
timschopinski <timschopinski@gmail.com>
2023-05-17 12:10:29 +0200

    add split test

--- app/tests/test_dataset.py
+++ app/tests/test_dataset.py
@@ -1,5 +1,5 @@
 import unittest
-from data.data_extractor import get_data
+from data.data_extractor import get_data, split
 from utils.enums import TimeFrame
 from pandas import DataFrame, DatetimeIndex
 
@@ -19,3 +19,9 @@ class DataExtractorTest(unittest.TestCase):
 
     def test_get_data_index_is_date(self):
         self.assertIsInstance(self.data.index, DatetimeIndex)
+
+    def test_data_split(self):
+        train_data, test_data = split(self.data, test_size=0.2)
+        self.assertIsInstance(train_data, DataFrame)
+        self.assertIsInstance(test_data, DataFrame)
+        self.assertLess(len(test_data), len(train_data))
