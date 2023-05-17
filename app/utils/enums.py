de85b679366129b2eee944095e09d737f3c18d1b
timschopinski <timschopinski@gmail.com>
2023-05-17 11:15:57 +0200

    save_chart fix

--- app/utils/enums.py
+++ app/utils/enums.py
@@ -1,4 +1,14 @@
+import argparse
 from enum import Enum
+
+
+class TimeFrameAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        try:
+            time_frame = TimeFrame(values.lower())
+            setattr(namespace, self.dest, time_frame)
+        except ValueError:
+            parser.error("Invalid time frame provided")
 
 
 class TimeFrame(Enum):
