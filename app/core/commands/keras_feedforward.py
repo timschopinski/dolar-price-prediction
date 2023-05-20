fae9019855a0e908a495c43872ed92f706102587
timschopinski <timschopinski@gmail.com>
2023-05-20 12:10:00 +0200

    add keras feedforward

---
+++ app/core/commands/keras_feedforward.py
@@ -0,0 +1,72 @@
+import logging
+from argparse import Namespace
+from core.data.visualization import inspect_data
+from core.management import BaseCommand, BaseCommandArgumentParser
+from core.data.mse import calculate_mse
+from core.data.data_extractor import get_data, split
+from matplotlib import pyplot as plt
+from core.utils.files import save_chart
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow import keras
+
+
+class Command(BaseCommand):
+    def __init__(self):
+        super().__init__()
+
+    def handle(self, *args, **kwargs):
+        args = self.get_parsed_args()
+        data = get_data(args.time_frame, args.date_from, args.date_to)
+
+        train_data, test_data = split(data, args.test_size)
+        inspect_data(train_data, self.logger, "Train Data")
+        inspect_data(test_data, self.logger, "Test Data")
+
+        scaler = MinMaxScaler()
+        train_data_scaled = scaler.fit_transform(train_data)
+        test_data_scaled = scaler.transform(test_data)
+
+        train_features = train_data_scaled[:, :-1]
+        train_target = train_data_scaled[:, -1]
+
+        model = keras.Sequential([
+            keras.layers.Dense(32, activation='relu', input_shape=(train_features.shape[1],)),
+            keras.layers.Dense(16, activation='relu'),
+            keras.layers.Dense(1)
+        ])
+        model.compile(optimizer='adam', loss='mean_squared_error')
+
+        model.fit(train_features, train_target, epochs=100, batch_size=16)
+
+        test_features = test_data_scaled[:, :-1]
+        predictions = model.predict(test_features)
+
+        predictions = scaler.inverse_transform(np.concatenate((test_features, predictions), axis=1))[:, -1]
+        mse = calculate_mse(predictions, test_data['Close'].values)
+        self.logger.info(f"MSE: {mse}")
+
+        # Plot the results
+        self.plot(test_data['Close'].values, predictions, args)
+
+    def plot(self, actual_values: np.ndarray, predictions: np.ndarray, args: Namespace):
+        plt.figure(figsize=(12, 6))
+        plt.plot(actual_values, label='Actual')
+        plt.plot(predictions, label='Predicted')
+        plt.title('USD/PLN Actual vs. Predicted Prices')
+        plt.xlabel('Time')
+        plt.ylabel('Price')
+        plt.title("Keras FeedForward Neural Networks")
+        plt.legend()
+        save_chart(args.title, self.logger)
+        plt.show()
+
+    def get_parsed_args(self) -> Namespace:
+        parser = BaseCommandArgumentParser(
+            description='Calculate price using Feedforward Neural Networks with keras'
+        )
+        parser.add_argument('--test_size', type=float, help='Test data size', default=0.2)
+        args, _ = parser.parse_known_args(title="feedforward-neural-network")
+        if args.verbose:
+            self.logger.setLevel(logging.INFO)
+        return args
---
+++ app/core/models/README.md
@@ -0,0 +1,15 @@
+
+
+# FeedForward
+
+
+The neural network used in the provided code is a feedforward neural network, specifically a multi-layer perceptron (MLP). Here's a breakdown of the neural network architecture:
+
+The input layer has the same number of neurons as the number of features in the data.
+There are two hidden layers with 32 and 16 neurons, respectively. These layers use the ReLU activation function, which introduces non-linearity to the model.
+The output layer consists of a single neuron, as the goal is to predict a single continuous value (USD/PLN price). There is no activation function applied to the output layer, making it a linear layer.
+The architecture of this neural network is sequential, meaning that the layers are stacked on top of each other sequentially. Each layer is fully connected to the next layer, meaning that all the neurons in a layer are connected to all the neurons in the subsequent layer.
+
+The model is trained using the mean squared error (MSE) loss function and the Adam optimizer. The MSE loss function is commonly used for regression tasks, aiming to minimize the squared difference between the predicted and actual values.
+
+Overall, this neural network architecture can be considered a basic MLP model suitable for regression problems like predicting USD/PLN prices.
