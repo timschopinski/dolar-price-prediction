2195955c0d88751b2552cd8e994e227dfcda905c
timschopinski <timschopinski@gmail.com>
2023-05-25 19:47:55 +0200

    update segmented regression

--- app/core/commands/segmented_regression.py
+++ app/core/commands/segmented_regression.py
@@ -1,13 +1,12 @@
 import logging
 from argparse import Namespace
-from typing import List
+from pandas import DataFrame
 from core.data.visualization import inspect_data
 from core.management import BaseCommand, BaseCommandArgumentParser
 from core.data.data_extractor import get_data
 from matplotlib import pyplot as plt
-from core.models.linear_regression import LinearRegression
+from core.models.segmented_regression import SegmentedRegression
 from core.utils.files import save_chart
-import numpy as np
 
 
 class Command(BaseCommand):
@@ -18,54 +17,15 @@ class Command(BaseCommand):
         args = self.get_parsed_args()
         data = get_data(args.time_frame, args.date_from, args.date_to)
         inspect_data(data, self.logger, "DATA")
+        model = SegmentedRegression(data)
 
-        segments = []
-        current_segment = [0]
-        for i in range(1, len(data)):
-            if data['Close'][i] < data['Close'][i - 1]:
-                segments.append(current_segment)
-                current_segment = [i]
-            else:
-                current_segment.append(i)
-        segments.append(current_segment)
+        actual_data, predicted_data = model.predict()
+        self.plot(actual_data, predicted_data, args)
 
-        segment_dates = []
-        segment_actuals = []
-        segment_prediction_dates = []
-        segment_predictions = []
-        for segment in segments:
-            segment_data = data.iloc[segment]
-            train_data = segment_data.iloc[:-1]
-
-            # Check if the segment has more than one row
-            if len(train_data) <= 0:
-                continue
-
-            test_data = segment_data.iloc[-1:]  # Use the last row for testing
-            model = LinearRegression(train_data)
-            y_pred = model.predict(test_data)
-            segment_prediction_dates.extend(test_data.index)
-            segment_dates.extend(segment_data.index)
-            segment_actuals.extend(segment_data['Close'])
-            segment_predictions.append(y_pred[0])
-
-        segment_prediction_dates = [date for date, prediction in zip(segment_prediction_dates, segment_predictions) if
-                                    not np.isnan(prediction)]
-        segment_predictions = [prediction for prediction in segment_predictions if not np.isnan(prediction)]
-
-        self.plot(segment_dates, segment_actuals, segment_prediction_dates, segment_predictions, args)
-
-    def plot(
-            self,
-            segment_dates: List,
-            segment_actuals: List,
-            segment_prediction_dates_num: List,
-            segment_predictions: List,
-            args: Namespace
-    ):
+    def plot(self, actual_data: DataFrame, predicted_data: DataFrame, args: Namespace):
         plt.figure(figsize=(12, 6))
-        plt.plot(segment_dates, segment_actuals, marker='o', label='Actual', c='#1f77b4')
-        plt.plot(segment_prediction_dates_num, segment_predictions, marker='o', label='Predicted', color='orange')
+        plt.plot(actual_data.index, actual_data['Close'], marker='o', label='Actual', c='#1f77b4')
+        plt.plot(predicted_data.index, predicted_data['Close'], marker='o', label='Predicted', color='orange')
         plt.xlabel('Date')
         plt.ylabel('Close Price')
         plt.title('Segmented Linear Regression')
--- app/core/models/segmented_regression.py
+++ app/core/models/segmented_regression.py
@@ -1,58 +1,70 @@
 from typing import List, Tuple
 import numpy as np
-from numpy import ndarray
+import pandas as pd
 from pandas import DataFrame
+from core.models.linear_regression import LinearRegression
 
 
-def get_segmented_regression(train_data: DataFrame) -> List[Tuple[float, float]]:
-    x_data = (train_data.index - train_data.index[0]).days
-    y_data = train_data['Close'].values
+def predict_price(data: DataFrame, segments: List) -> Tuple[DataFrame, DataFrame]:
+    segment_dates = []
+    segment_actuals = []
+    segment_prediction_dates = []
+    segment_predictions = []
+    for segment in segments:
+        segment_data = data.iloc[segment]
+        train_data = segment_data.iloc[:-1]
 
-    # Podział danych na segmenty
+        # Check if the segment has more than one row
+        if len(train_data) <= 0:
+            continue
+
+        test_data = segment_data.iloc[-1:]
+        model = LinearRegression(train_data)
+        y_pred = model.predict(test_data)
+        segment_prediction_dates.extend(test_data.index)
+        segment_dates.extend(segment_data.index)
+        segment_actuals.extend(segment_data['Close'])
+        segment_predictions.append(y_pred[0])
+
+    segment_prediction_dates = [date for date, prediction in zip(segment_prediction_dates, segment_predictions) if
+                                not np.isnan(prediction)]
+    segment_predictions = [prediction for prediction in segment_predictions if not np.isnan(prediction)]
+    actual_data_dict = {
+        'Date': segment_dates,
+        'Close': segment_actuals
+    }
+    predictions_data_dict = {
+        'Date': segment_prediction_dates,
+        'Close': segment_predictions
+    }
+    print(actual_data_dict)
+    print(len(actual_data_dict["Date"]))
+    print(len(actual_data_dict["Close"]))
+
+    # Create a dataframe
+    actual_data = pd.DataFrame(actual_data_dict, index=actual_data_dict["Date"])
+    predicted_data = pd.DataFrame(predictions_data_dict, index=predictions_data_dict["Date"])
+    return actual_data, predicted_data
+
+
+def get_segments(data: DataFrame) -> List:
     segments = []
-    current_segment = [0]  # Indeksy początków segmentów
-    for i in range(1, len(y_data)):
-        if np.sign(y_data[i] - y_data[i-1]) != np.sign(y_data[i-1] - y_data[i-2]):
+    current_segment = [0]
+    for i in range(1, len(data)):
+        if data['Close'][i] < data['Close'][i - 1]:
+            segments.append(current_segment)
+            current_segment = [i]
+        else:
             current_segment.append(i)
-    current_segment.append(len(y_data))
-
-    # Dopasowanie regresji liniowej do każdego segmentu
-    regression_params = []
-    for i in range(len(current_segment) - 1):
-        start = current_segment[i]
-        end = current_segment[i+1]
-        segment_x = x_data[start:end]
-        segment_y = y_data[start:end]
-        segment_a, segment_b = get_closed_form_solution(segment_x, segment_y)
-        regression_params.append((segment_a, segment_b))
-
-    return regression_params
-
-
-def get_closed_form_solution(x_data: np.ndarray, y_data: np.ndarray) -> Tuple[float, float]:
-    x_mean = np.mean(x_data)
-    y_mean = np.mean(y_data)
-    xy_mean = np.mean(x_data * y_data)
-    x_squared_mean = np.mean(x_data ** 2)
-    a = (xy_mean - x_mean * y_mean) / (x_squared_mean - x_mean ** 2)
-    b = y_mean - a * x_mean
-    return a, b
-
-
-def predict_price(test_data: DataFrame, train_data: DataFrame, regression_params: List[Tuple[float, float]]) -> ndarray:
-    x_test = (test_data.index - train_data.index[0]).days
-    y_pred = np.zeros_like(x_test, dtype=float)
-    for segment_a, segment_b in regression_params:
-        segment_pred = segment_a * x_test + segment_b
-        y_pred += np.where(x_test >= 0, segment_pred, 0)  # Unikanie ujemnych prognoz
-    return y_pred
+    segments.append(current_segment)
+    return segments
 
 
 class SegmentedRegression:
 
-    def __init__(self, train_data: DataFrame):
-        self.train_data = train_data
-        self.regression_params = get_segmented_regression(self.train_data)
+    def __init__(self, data: DataFrame):
+        self.data = data
+        self.segments = get_segments(data)
 
-    def predict(self, test_data: DataFrame):
-        return predict_price(test_data, self.train_data, self.regression_params)
+    def predict(self):
+        return predict_price(self.data, self.segments)
